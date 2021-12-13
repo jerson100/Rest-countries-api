81d931280313b451d05edf91e6399e1c182aeb6a
jerson100 <juamkoo@gmail.com>
2021-12-13 18:35:51 -0500

    Agregando los archivos iniciales.

---
+++ src/views/HomeView/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HomeViewContainerStyle } from "./homeView.style";
+
+const HomeView = () => {
+  return (
+    <HomeViewContainerStyle>
+      <p>Bienvenido...</p>
+    </HomeViewContainerStyle>
+  );
+};
+
+export default HomeView;
