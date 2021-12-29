7913c1c8b36a798bd5d284e859356cbb09fb39a2
jerson100 <juamkoo@gmail.com>
2021-12-28 20:00:32 -0500

    Se terminó el componente del botón back

--- src/views/CountryDetails/CountryDetails.jsx
+++ src/views/CountryDetails/CountryDetails.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ButtonBack from "../../components/common/ButtonBack";
 
 const CountryDetails = () => {
-  return <div>aaaa</div>;
+  return (
+    <div>
+      <ButtonBack />
+    </div>
+  );
 };
 
 export default CountryDetails;
